d887b3c6043b520b4773d23a0d2a2b48243397f8
Resoluter <xujabekovsirojiddin@gmail.com>
2022-03-12 23:20:08 +0500

    commit

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,34 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Resoluter/bookstore-users.git/domain/users"
+	"github.com/Resoluter/bookstore-users.git/services"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	// fmt.Println(user)
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	return
+	// }
+	// if err := json.Unmarshal(bytes, &user); err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/Resoluter/bookstore-users.git/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
